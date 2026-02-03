0a4ded360e8060457b2f4ae7904f79a0e2d35067
Deepak Suthar <deepak2003suthar@gmail.com>
2026-02-03 07:47:59 +0530

    corrections

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -304,7 +304,7 @@ export default function WishlistPage() {
 
                 {/* Wishlist Items Grid */}
                 {!isLoading && !error && wishlistItems.length > 0 && (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6">
                         {wishlistItems.map((item) => (
                             <div
                                 key={item._id}
